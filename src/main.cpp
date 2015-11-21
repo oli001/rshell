39c154eba08e8a156ea6d06a07c0d2934cd791eb
Owen Li <oli001@hammer.cs.ucr.edu>
2015-11-20 17:48:58 -0800

    fixed unsigned to signed comparison

--- src/main.cpp
+++ src/main.cpp
@@ -236,7 +236,7 @@ vector<string> tokenize(string x)
 vector<string> separateByDelim(vector<string> origVector, string delimiter)
 {
     vector<string> tokenVectorFinal;
-    int pos = 0;
+    unsigned pos = 0;
     string oldString;
     string newString;
     for(unsigned l = 0; l < origVector.size(); l++)
